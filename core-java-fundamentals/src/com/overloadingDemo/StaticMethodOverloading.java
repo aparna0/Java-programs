d1543c933e3b2e6e4ffc75eb7e16a791975e4917
Aparna Mangalaram <45903029+aparna0@users.noreply.github.com>
2023-12-17 23:50:28 +0530

    Update StaticMethodOverloading.java

--- core-java-fundamentals/src/com/overloadingDemo/StaticMethodOverloading.java
+++ core-java-fundamentals/src/com/overloadingDemo/StaticMethodOverloading.java
@@ -15,3 +15,13 @@ public class StaticMethodOverloading {
 	}
 
 }
+
+=============Main method overloading================
+public class Main {
+    public static void main(String[] args) {
+        main();
+    }
+    public static void main(){
+        System.out.println("ok");
+    }
+}
