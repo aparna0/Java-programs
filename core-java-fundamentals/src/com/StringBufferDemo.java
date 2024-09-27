f273830b2c464990ee3496d5f62694243eba7166
Aparna Mangalaram <45903029+aparna0@users.noreply.github.com>
2024-09-27 07:39:26 +0530

    Update StringBufferDemo.java

--- core-java-fundamentals/src/com/StringBufferDemo.java
+++ core-java-fundamentals/src/com/StringBufferDemo.java
@@ -2,69 +2,86 @@ package com;
 
 public class StringBufferDemo {
 	public static void main(String args[]) {
-		StringBuffer str1 = new StringBuffer();
-		StringBuffer str2 = new StringBuffer("aparna");
-		StringBuffer str3 = new StringBuffer("aparna");
-		StringBuffer str4 = new StringBuffer("abc");
-		StringBuffer str5 = new StringBuffer(4);
-		System.out.println("Str1 : "+str1);
-		System.out.println("Str2 : "+str2);
-		System.out.println("Str3 : "+str3);
-		System.out.println("Str4 : "+str4);
-		System.out.println("str5 : "+str5);
-		
-		System.out.println("is Str2 and str3 equals ? "+str2.equals(str3)+" because equals compares reference(memory location) not a values");
-		System.out.println("hashcode of Str1 : "+str1.hashCode());
-		System.out.println("hashcode of Str2 : "+str2.hashCode());
-		System.out.println("hashcode of Str3 : "+str3.hashCode());
-		System.out.println("hashcode of Str4 : "+str4.hashCode());
-		
-		System.out.println("default capacity of any StringBUffer is "+str1.capacity());
-		System.out.println("capacity of str2 is "+str2.capacity()+" i.e default 16+6(aparna)=22");
-		System.out.println("capacity of str4 is "+str4.capacity()+" i.e default 16+3(abc)=19");
-		str1.append("abc");
-		System.out.println("Str1 : "+str1+" and capacity : "+str1.capacity());
-		str1.append(" cgfggfgjfghfhg ");
-		System.out.println("Str1 : "+str1+" and capacity : "+str1.capacity());
-		str2.append(" chakrapani mangalaram");
-		System.out.println("Str1 : "+str2+" and capacity : "+str2.capacity());
-		str5.insert(0,"xyz");
-		System.out.println("Str5 : "+str5+" and capacity : "+str5.capacity());
-		str5.append(" this is demo");
-		System.out.println("Str5 : "+str5+" and capacity : "+str5.capacity());
+		//StringBuffer is mutable and thread safe
+        
+        StringBuffer sb1 = new StringBuffer();
+        System.out.println("Default capacity is "+sb1.capacity());      //16
 
-		StringBuffer str1 = new StringBuffer("abc");
-	        StringBuffer str2 = new StringBuffer("abc");
-	
-	        System.out.println("str1: "+str1);
-	        System.out.println("str2: "+str2);
-	
-	        System.out.println("is str1 equals to str2? "+str1.equals(str2));	//false
-	        System.out.println("is str1 == str2? "+ (str1==str2));			//fakse
-		
-		/*
-		str3.append(" CM");
-		System.out.println("str3 is "+str3+" and capacity is "+str3.capacity());
-		System.out.println("capacity of str5 is "+str5.capacity()+" because explicitly specified");
-		System.out.println("inserting value 'abc' in str5");
-		str5.append("abc ");
-		System.out.println("capacity of str5 is "+str5.capacity());
-		System.out.println("inserting value(orchid college) in str5");
-		str5.insert(0,"orchid college");
-		System.out.println("capacity of str5 after insertion is "+str5.capacity()+" because legth of string is 14");
-		System.out.println("inserting abc in str5");
-		str5.append(" abc");
-		System.out.println("str5 : "+str5+" capacity : "+str5.capacity());
-		
-		str1.insert(0,"chakrapani mangalaram");
-		System.out.println(str1);
-		str2.append(str1);
-		System.out.println("now str 2 :"+str2+" and hashcode : "+str2.hashCode()+" and capacity is "+str2.capacity());
-		System.out.println(str3.reverse());
-		System.out.println(str3);
-		System.out.println("Str1 :"+str1+" capcity : "+str1.capacity());
-		str1.append("abcg");
-		System.out.println("Str1 :"+str1+" capcity : "+str1.capacity());
-		*/
+        //extending capacity, we add more char then it will increase capacity by
+        //capacity = (current capacity + 1) * 2
+        sb1.append("11111222223333344444");
+        System.out.println("capacity is "+sb1.capacity());              //34 <= (16+1)*2
+
+        StringBuffer sb2 = new StringBuffer("abc");
+        //if you create StringBuffer with given String then
+        //capacity = default capacity + length if passed string
+        System.out.println("capacity is "+sb2.capacity());              //19 <= 16+3
+
+        sb2.append("12345678912345678910");
+        System.out.println("capacity is "+sb2.capacity());              //40 <= (current+1)*2
+
+        StringBuffer sb3 = new StringBuffer("123456789123456789");       //34 <= 16+18
+        System.out.println("capacity is "+sb3.capacity());
+
+        //Important methods of StringBuffer
+        //capacity()
+        //length()
+        //append(x)  // x = int, float, double, long, char, string, char[], StringBuilder
+        //charAt()
+        //setCharAt()
+        //indexOf(str) / indexOf(string str, int fromIndex)
+        //insert(off, any datatype value)
+        //delete(fromIndex, number of char)
+        //deleteCharAt(index)
+        //setLength(int length)
+        //ensureCapacity(int minCapacity)
+        //trimToSize()
+
+        System.out.println(sb3.length());           //18
+
+        System.out.println(sb3.append(123));
+        System.out.println(sb3.append(123.5f));
+        System.out.println(sb3.append(123.56));
+        char[] ch = {'z','v','k'};
+        System.out.println(sb3.append(ch,1, 2));    //123456789123456789123123.5123.56vk
+
+        sb3.setCharAt(1,'x');                       //x
+
+        System.out.println(sb3.charAt(1));                      //2
+        System.out.println(sb3);                                //1x3456789123456789123123.5123.56vk
+
+        System.out.println(sb3.indexOf("1"));                   //0
+        System.out.println(sb3.indexOf("1", 9));    //9
+
+        System.out.println(sb1.insert(1,"aparna"));
+        System.out.println(sb1.delete(0,sb1.length()));       //delete whole valur of StringBuffer
+        //System.out.println(sb1.insert(1,"aparna"));           //StringIndexOutOfBoundsException
+
+        sb1.append(12345);
+
+        sb1.delete(0,2);
+        System.out.println(sb1);        //345
+
+        sb1.deleteCharAt(0);
+        System.out.println(sb1);        //45
+
+        StringBuffer sb4 = new StringBuffer("123456789");
+        sb4.setLength(5);
+        System.out.println(sb4);        //12345
+        sb4.append(9876);
+        System.out.println(sb4);        //123459876
+
+        StringBuffer sb5 = new StringBuffer();  //default 16
+        sb5.ensureCapacity(3);
+        System.out.println(sb5.capacity());     //16 <- current capacity, because ensure capacity is less that
+        sb5.ensureCapacity(17);
+        System.out.println(sb5.capacity());     //34 <- (16+1)*2 <- (current Capacity + 1) * 2 , because current capacity is less that ensure capacity
+        sb5.append("1111111111222222222233333333334444444444");
+        System.out.println(sb5.capacity());     //70 <- (current Capacity + 1) * 2
+
+        StringBuffer sb6 = new StringBuffer(20);
+        sb6.append(123);
+        sb6.trimToSize();
+        System.out.println(sb6.capacity());     //3
 	} 
 }
