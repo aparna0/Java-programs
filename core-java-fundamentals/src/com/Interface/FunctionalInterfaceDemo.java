ec087cfb7584378d4961d654059d298b5ae62b17
Aparna Mangalaram <45903029+aparna0@users.noreply.github.com>
2025-03-09 16:32:58 +0530

    Update FunctionalInterfaceDemo.java

--- core-java-fundamentals/src/com/Interface/FunctionalInterfaceDemo.java
+++ core-java-fundamentals/src/com/Interface/FunctionalInterfaceDemo.java
@@ -1,21 +1,31 @@
 package com.Interface;
 
-//functional interface is interface with only one method
+//functional interface is interface with only one method but can have multiple default and static methods
+//function interface annotated with @FunctionalInterface(optional) to ensure interface doesn't breaks functional interface rule.
 interface FunInterface{
 	void display();
+	default void defaultMethod(){
+		System.out.println("it is default methhod");
+	}
+	static void StaticMethod(){
+		System.out.println("it is default methhod");
+	}
 }
 public class FunctionalInterfaceDemo {
 
 	public static void main(String[] args) {
 		FunInterface in1 = new FunInterface() {
-			public void display() {
-				System.out.println("function interface is implemented by annonymous class");
-			}
+		public void display() {
+			System.out.println("function interface is implemented by annonymous class");
+		}
 		};
 		in1.display();
+		FunInterface.staticMethod();
+		
 		
 		FunInterface in2 = () -> System.out.println("function interface is implemented by annonymous class with lambda expression");
 		in2.display();
+        	in2.defaultMethod();
 	}
 
 }
